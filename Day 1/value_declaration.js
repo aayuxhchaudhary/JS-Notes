bfbfba7d94ae80c0e73a73c99ffac6cbb4bd21f8
Aayush <aayushc951@gmail.com>
2026-06-16 16:55:48 +0530

    let,var,const

---
+++ Day 1/value_declaration.js
@@ -0,0 +1,13 @@
+let n = 10
+n=20
+
+var m = 10
+m =20 
+
+const o = 10
+// o=20 // This will cause an error because const variables cannot be reassigned
+
+
+console.log(n) // Output: 20
+console.log(m) // Output: 20
+console.log(o) // Output: 10
