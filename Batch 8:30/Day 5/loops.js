48095171ceadaae76b58f49b0ae15f010263a875
Aayush <aayushc951@gmail.com>
2026-06-26 09:06:27 +0530

    loops

---
+++ Batch 8:30/Day 5/loops.js
@@ -0,0 +1,16 @@
+// For loop, While Loop, Do-While loop
+
+// Print even number from 1 to 20
+// for(let i = 1;i<=20;i++){
+//     if(i%2!=0){
+//         console.log(i)
+//     }
+// }
+
+// let m = "aeiou";
+
+// for (let i = 0; i <= m.length - 1; i++) {
+//   if (m[i] == "a" || m[i] == "e" || m[i] == "i" || m[i] == "o" || m[i] == "u") {
+//     console.log(m[i])
+//   }
+// }
