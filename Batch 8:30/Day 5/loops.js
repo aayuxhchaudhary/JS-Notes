13e6ee791c7abfc6169fb9e57144e89fc1e84ce8
Aayush <aayushc951@gmail.com>
2026-06-26 09:09:11 +0530

    day 5 assignment

--- Batch 8:30/Day 5/loops.js
+++ Batch 8:30/Day 5/loops.js
@@ -14,3 +14,14 @@
 //     console.log(m[i])
 //   }
 // }
+
+
+// Q1. Print table of 3 using loop
+// Q2. Print vowels from a string value, both upper and lower case
+// Q3. Print consonents from a string value, upper case or lower case
+// Q4. Print a number pattern
+// 1 1 1 1 1
+// 2 2 2 2 2
+// 3 3 3 3 3
+// 4 4 4 4 4
+// 5 5 5 5 5
